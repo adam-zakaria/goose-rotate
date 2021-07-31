3954303a7ea226f7c54ecaec682f3b5a9fa5eab2
Adam Zakaria <adam.p.zakaria@gmail.com>
2021-07-31 16:21:48 -0400

    rotate attempt

--- gen.js
+++ gen.js
@@ -1,15 +1,3 @@
-/*
-function setup() {
-  // here we use a callback to display the image after loading
-  loadImage('duck.png', img => {
-    //image(img, 100, 100);
-    image(img, 0, 0);
-    img.resize(0,1000);
-
-  });
-}
-*/
-
 var canvas = document.getElementById('canvas'),
 context = canvas.getContext('2d');
 
@@ -26,31 +14,10 @@ function make_base()
   j = 0;
   
   base_image.onload = function(){
-      rotation = 0;
-      //context.drawImage(base_image, 0, 0, 100, 100); //offsetx,y height x,y
-      
-    for(var i = 0; i <= 0; i++){
-      for(var j = 0; j <= 4; j++){
-        translateX = j*200;
-        translateY = i*200;
-        /* draw, translate, rotate, translate back */
-        //context.translate(translateX,translateY);
-        context.translate(translateX + 100*.5,translateY+50*.5);
-        context.rotate(rotation * Math.PI / 180);
-        context.translate(-1*(translateX + 100*.5),-1*(translateY+50*.5));
-        context.drawImage(base_image, 0, 0, 100, 50); //offsetx,y height x,y
-        //context.fillText(j, 0, 0, 2000);
-        context.fillText(j, 0, 0);
-        context.restore();
-        context.save();
-        //rotation += 20;
-        rotation += 90;
-        console.log(`rotation: ${rotation}`);
-        console.log(`translateX: ${translateX}`);
-        console.log(`translateY: ${translateY}`);
-        console.log(`j: ${j}`);
-        console.log('---------------');
-      }
-    }
+    context.draw_image(base_image, 0, 0, 100, 50);
+    context.translate(300 + 50, 300 + 25); //x + .5*width, y + .5*height
+    context.rotate((Math.PI / 180) * 25); // rotate
+    context.translate(-350, -325); //x + .5*width, y + .5*height
+    draw_image(base_image, 0, 0, 100, 50);
   }
 }
