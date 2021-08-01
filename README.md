c9fcecff2fe2163aa7800d0a77f84070404bc665
Adam Zakaria <adam.p.zakaria@gmail.com>
2021-08-01 16:00:51 -0400

    Aligned the geese within the canvas, with padding

--- README.md
+++ README.md
@@ -6,4 +6,11 @@ red_rectange / other images might be problematic when rotated because they're no
 There's also this matter of rotating a shape around its own center.
 I think rotating around its top left corner works great though...
 
+T-shirt to print on:
 Bella Canvas 3001
+
+Debugging using mouse position:
+Enter this in the console:
+var x, y; document.onmousemove=(e)=>{x=e.pageX;y=e.pageY;}
+Enter this in as a live expression:
+"("+x+", "+y+")"
--- gen.js
+++ gen.js
@@ -7,26 +7,20 @@ function make_base()
 {
   base_image = new Image();
   base_image.src = 'goose.png';
-  //base_image.src = 'duck.png';
-  //base_image.src = 'red rectangle cropped.png';
-  //context.translate(100,100);
   context.save();
   rotate = 0;
   base_image.onload = function(){
-    //context.fillStyle = '#0095DD';
     for(i=0; i<=4; i++){
       for(j=0; j<=4; j++){
-        startX = 200*i+20;
-        startY = 200*j+20;
+        startX = 200*j+20;
+        startY = 200*i+20;
         width = 100 * base_image.height / base_image.width;
+        console.log(`width: ${width}`);
         height = 100;
         rotate += 30;
         context.translate(startX + .5*width, startY + .5*height); //x + .5*width, y + .5*height
         context.rotate((Math.PI / 180) * rotate); // rotate
         context.translate(-1 * (startX + .5*width), -1 * (startY + .5*height)); //x + .5*width, y + .5*height
-        //context.translate(-100, -75); //x + .5*width, y + .5*height
-        //context.fillStyle = '#4D4E53';
-        //context.fillRect(startX, startY, width, height);
         context.drawImage(base_image, startX, startY, 100, width);
         context.restore();
         context.save();
