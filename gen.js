96b1cafec0e857290745a5bae1c83d013b7c9148
Adam Zakaria <adam.p.zakaria@gmail.com>
2021-07-31 22:32:46 -0400

    Rotated geese

--- gen.js
+++ gen.js
@@ -6,10 +6,10 @@ make_base();
 function make_base()
 {
   base_image = new Image();
+  base_image.src = 'duck crop.png';
   //base_image.src = 'duck.png';
-  //base_image.src = 'duck.png';
-  base_image.src = 'red rectangle cropped.png';
-  context.translate(100,100);
+  //base_image.src = 'red rectangle cropped.png';
+  //context.translate(100,100);
   context.save();
   rotate = 0;
   base_image.onload = function(){
@@ -18,17 +18,20 @@ function make_base()
       for(j=0; j<=4; j++){
         startX = 200*i + 200;
         startY = 200*j + 200;
-        rectWidth = 100;
-        rectHeight = 50;
+        width = 100 * base_image.height / base_image.width;
+        height = 100;
         rotate += 25;
-        context.translate(startX + .5*rectWidth, startY + .5*rectHeight); //x + .5*width, y + .5*height
+        context.translate(startX + .5*width, startY + .5*height); //x + .5*width, y + .5*height
         context.rotate((Math.PI / 180) * rotate); // rotate
-        context.translate(-1 * (startX + .5*rectWidth), -1 * (startY + .5*rectHeight)); //x + .5*width, y + .5*height
+        context.translate(-1 * (startX + .5*width), -1 * (startY + .5*height)); //x + .5*width, y + .5*height
         //context.translate(-100, -75); //x + .5*width, y + .5*height
-        context.fillStyle = '#4D4E53';
-        context.fillRect(startX, startY, rectWidth, rectHeight);
+        //context.fillStyle = '#4D4E53';
+        //context.fillRect(startX, startY, width, height);
+        context.drawImage(base_image, startX, startY, 100, width);
         context.restore();
         context.save();
+        console.log(`startX = ${startX}`);
+        console.log(`startY = ${startY}`);
       }
     }
   }
